d93e895a5c833639078e7ce5ee7a0d4620ccc0e6
Thomas Massmann <thomas@propertyshelf.com>
2014-11-17 20:23:15 +0100

    Added setup tests, custom browser layer.

---
+++ src/ps/plone/fotorama/tests/test_setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Test Setup of ps.plone.fotorama."""
+
+# python imports
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# zope imports
+from plone import api
+from plone.browserlayer.utils import registered_layers
+
+# local imports
+from ps.plone.fotorama.config import PROJECTNAME
+from ps.plone.fotorama.testing import INTEGRATION_TESTING
+
+
+class TestSetup(unittest.TestCase):
+    """Validate setup process for ps.plone.fotorama."""
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        """Additional test setup."""
+        self.portal = self.layer['portal']
+
+    def test_product_is_installed(self):
+        """Validate that our product is installed."""
+        qi = self.portal.portal_quickinstaller
+        self.assertTrue(qi.isProductInstalled(PROJECTNAME))
+
+    def test_addon_layer(self):
+        """Validate that the browserlayer for our product is installed."""
+        layers = [l.getName() for l in registered_layers()]
+        self.assertIn('IPloneFotoramaLayer', layers)
+
+
+class UninstallTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        """Additional test setup."""
+        self.portal = self.layer['portal']
+
+        qi = self.portal.portal_quickinstaller
+        with api.env.adopt_roles(['Manager']):
+            qi.uninstallProducts(products=[PROJECTNAME])
+
+    def test_product_is_uninstalled(self):
+        """Validate that our product is uninstalled."""
+        qi = self.portal.portal_quickinstaller
+        self.assertFalse(qi.isProductInstalled(PROJECTNAME))
+
+    def test_addon_layer_removed(self):
+        """Validate that the browserlayer is removed."""
+        layers = [l.getName() for l in registered_layers()]
+        self.assertNotIn('IPloneFotoramaLayer', layers)
