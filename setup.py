28d0a5bd1a9a2015e1398980f4cba1bda77fbad9
Thomas Massmann <thomas@propertyshelf.com>
2014-11-21 13:31:18 +0100

    Back to development. [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '4.6.2'
+version = '4.6.3dev'
 description = 'Fotorama slideshow for Plone.'
 long_description = ('\n'.join([
     open('README.rst').read(),
